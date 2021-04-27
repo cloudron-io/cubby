45c82f2ce5026de435eff9456593dbb81bde09e4
Johannes Zellner <johannes@nebulon.de>
2021-04-27 16:38:12 +0200

    Update package

--- start.sh
+++ start.sh
@@ -10,5 +10,8 @@ chown -R cloudron:cloudron /app/data
 echo "=> Run db-migration"
 DATABASE_URL=${CLOUDRON_POSTGRESQL_URL} /app/code/node_modules/.bin/db-migrate up
 
+echo "=> Ensure admin account with admin:admin"
+/app/code/cli.js user-add --username admin --password admin --email admin@server.local --display-name Admin || true
+
 echo "=> Start the server"
 exec /usr/local/bin/gosu cloudron:cloudron node /app/code/app.js
