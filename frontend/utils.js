17ccd0a58ecfb2da3090a72603872d396db52a93
Johannes Zellner <johannes@nebulon.de>
2021-05-11 17:04:34 +0200

    Shorten minutes to min

--- frontend/utils.js
+++ frontend/utils.js
@@ -11,8 +11,8 @@ function prettyDate(value) {
 
     return day_diff === 0 && (
         diff < 60 && 'just now' ||
-        diff < 120 && '1 minute ago' ||
-        diff < 3600 && Math.floor( diff / 60 ) + ' minutes ago' ||
+        diff < 120 && '1 min ago' ||
+        diff < 3600 && Math.floor( diff / 60 ) + ' min ago' ||
         diff < 7200 && '1 hour ago' ||
         diff < 86400 && Math.floor( diff / 3600 ) + ' hours ago') ||
         day_diff === 1 && 'Yesterday' ||
