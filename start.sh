0a2131392ccf759cea54dda15a8b101cd6984e8c
Johannes Zellner <johannes@nebulon.de>
2021-06-03 19:04:09 +0200

    The package now has ldap no need for admin account

--- start.sh
+++ start.sh
@@ -15,9 +15,6 @@ chown -R cloudron:cloudron /app/data
 echo "=> Run db-migration"
 DATABASE_URL=${CLOUDRON_POSTGRESQL_URL} /app/code/node_modules/.bin/db-migrate up
 
-echo "=> Ensure admin account with admin:admin"
-/app/code/cli.js user-add --username admin --password admin --email admin@server.local --display-name Admin || true
-
 echo "=> Start the server"
 export DEBUG="cubby:*"
 exec /usr/local/bin/gosu cloudron:cloudron node /app/code/app.js
