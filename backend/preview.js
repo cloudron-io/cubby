7e54b49cf3f5a53a96fd630e469db6fd1c7449cc
Johannes Zellner <johannes@nebulon.de>
2021-12-28 11:38:13 +0100

    Add basic preview task queue

--- backend/preview.js
+++ backend/preview.js
@@ -3,6 +3,7 @@
 var assert = require('assert'),
     constants = require('./constants.js'),
     crypto = require('crypto'),
+    debug = require('debug')('cubby:preview'),
     exec = require('util').promisify(require('child_process').exec),
     fs = require('fs-extra'),
     path = require('path');
@@ -12,17 +13,46 @@ exports = module.exports = {
     getLocalPath
 };
 
+const queue = [];
+let taskActive = false;
+
+async function process() {
+    if (!queue.length || taskActive) return;
+
+    taskActive = true;
+    const task = queue.shift();
+
+    debug(`process: hash=${task.hash} fullFilePath:${task.fullFilePath}`);
+
+    await task.generator(task.hash, task.fullFilePath);
+    taskActive = false;
+
+    // process next
+    process();
+}
+
+function enqueue(hash, fullFilePath, generator) {
+    if (queue.find(function (t) { return t.hash === hash; })) return;
+
+    debug(`enqueue: hash=${hash} fullFilePath:${fullFilePath}`);
+
+    queue.push({ hash, fullFilePath, generator });
+    process();
+}
+
 const generators = [{
     name: 'imagemagick',
     mimeTypes: [ 'image/jpeg' ],
-    generate: function (fullFilePath) {
+    getHash: function (fullFilePath) {
         assert.strictEqual(typeof fullFilePath, 'string');
 
         const hash = crypto.createHash('md5').update(fullFilePath).digest('hex');
+        const targetPath = path.join(constants.THUMBNAIL_ROOT, hash);
 
-        async function createIfNotExists() {
+        async function generate(hash, fullFilePath) {
             const targetPath = path.join(constants.THUMBNAIL_ROOT, hash);
-            if (fs.existsSync(targetPath)) return;
+
+            debug(`generateImageMagick: hash=${hash} fullFilePath=${fullFilePath}`);
 
             try {
                 await fs.ensureDir(constants.THUMBNAIL_ROOT);
@@ -32,10 +62,10 @@ const generators = [{
             }
         }
 
-        createIfNotExists();
+        if (!fs.existsSync(targetPath)) enqueue(hash, fullFilePath, generate);
 
         return hash;
-    }
+    },
 }];
 
 function getHash(mimeType, fullFilePath) {
@@ -45,7 +75,7 @@ function getHash(mimeType, fullFilePath) {
     const generator = generators.find(function (g) { return g.mimeTypes.indexOf(mimeType) !== -1; });
     if (!generator) return null;
 
-    return generator.generate(fullFilePath);
+    return generator.getHash(fullFilePath);
 }
 
 function getLocalPath(hash) {
