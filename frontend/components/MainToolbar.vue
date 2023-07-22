4b811c93b5ec6e16ec28bb0ba1bd08b04f65eaf6
Johannes Zellner <johannes@nebulon.de>
2023-07-22 19:18:18 +0200

    Bring back MainToolbar

--- frontend/components/MainToolbar.vue
+++ frontend/components/MainToolbar.vue
@@ -1,5 +1,42 @@
 <template>
+    <Toolbar>
+        <template #start>
+          <Button icon="pi pi-chevron-left" class="p-button-sm p-button-text" :disabled="breadCrumbs.length === 0" @click="onUp"/>
+          <Breadcrumb :home="breadCrumbHome" :model="breadCrumbs"/>
+        </template>
 
+        <template #end>
+            <!-- file action buttons -->
+            <div class="file-actions">
+                <Button v-show="selectedEntries.length > 1" icon="pi pi-download" class="p-button-outlined p-button-sm" v-tooltip="'Download'" @click="onDownload"/>
+                <Button v-show="displayName && selectedEntries.length > 1" icon="pi pi-trash" class="p-button-outlined p-button-sm p-button-danger" v-tooltip="'Delete'" @click="onDelete"/>
+            </div>
+
+            <!-- Always visible if we have a login session-->
+            <Button v-show="displayName" icon="pi pi-upload" class="p-button-sm" label="Upload" @click="onToggleMenuUpload"/>
+            <Button v-show="displayName" icon="pi pi-plus" class="p-button-sm" label="New" @click="onToggleMenuNew"/>
+
+            <div class="profile-actions">
+                <Button v-show="displayName" icon="pi pi-user" class="p-button-sm p-button-secondary" @click="onToggleMenuMain" :label="displayName"/>
+
+                <Menu ref="menuUpload" :model="uploadMenu" :popup="true"/>
+                <Menu ref="menuNew" :model="newMenu" :popup="true"/>
+                <Menu ref="menuMain" :model="mainMenu" :popup="true"/>
+            </div>
+        </template>
+    </Toolbar>
+
+    <!-- About Dialog -->
+    <Dialog header="About Cubby" v-model:visible="aboutDialog.visible" :dismissableMask="true" :closable="true" :style="{width: '450px'}" :modal="true">
+        <div>
+            Cubby the painless file sharing solution!<br/>
+            Developed by <a href="https://cloudron.io" target="_blank">Cloudron</a>.
+            <br/>
+        </div>
+        <template #footer>
+            <Button label="Close" icon="pi pi-times" class="p-button-text" @click="aboutDialog.visible = false"/>
+        </template>
+    </Dialog>
 </template>
 
 <script>
